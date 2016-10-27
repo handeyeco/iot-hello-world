59360c5f71630a774cf05f8ca852633303c36f51
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-10-26 20:26:07 -0700

    Refactor and comment

--- device/main.js
+++ device/main.js
@@ -1,30 +1,39 @@
+// Import modules
 const wifi = require('Wifi');
 const http = require('http');
 
+// Import WiFi credentials
 const {ssid, pw} = require('./private/wifi.js');
 
-let interval = 5000,
-    color = null;
+// State of LED color
+let color = null;
 
+// Information on where to pull
+// JSON object for LED state
 let httpOptions = {
   host: 'iot-hello-world.herokuapp.com',
   path: '/color',
   method: 'GET'
 };
 
+
+// Connect to WiFi
 wifi.connect(ssid, { password: pw }, error => {
   if (error) {
     console.error("WiFi error: " + error)
   } else {
-    console.log("Connected to WiFi");
+    console.log("Connected to WiFi.");
     getColor();
   };
 });
 
+
+// Make HTTP GET request for LED state JSON
 function getColor() {
   http.get(httpOptions, (res) => {
     console.log("HTTP get...");
 
+    // When data is returned update LED color state
     res.on('data', (data) => {
       data = JSON.parse(data);
       color = data.color;
@@ -33,18 +42,24 @@ function getColor() {
   });
 }
 
+
+// Update the physical LEDs
 function setLEDColor() {
   console.log("Setting color to " + color);
 
+  // If truthy...
   let blue  = color === "blue",
       green = color === "green",
       red   = color === "red";
 
-  digitalWrite(D0, blue);
-  digitalWrite(D2, green);
-  digitalWrite(D4, red);
+  // ...light LED
+  digitalWrite(D0, blue);   // D3 on board
+  digitalWrite(D2, green);  // D4 on board
+  digitalWrite(D4, red);    // D2 on board
 }
 
+
+// Interval to check/update color state
 setInterval(() => {
     getColor();
-}, interval);
+}, 2000);
